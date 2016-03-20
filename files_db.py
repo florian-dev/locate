f54d43be1a36b5190b597f285d10db1dd15ba7bc
florian-dev <florian-dev@users.noreply.github.com>
2016-03-20 20:28:07 +0100

    tests update

--- files_db.py
+++ files_db.py
@@ -10,8 +10,7 @@ class FilesDb:
 	def __init__(self, exclude_drives=('C',), db_filepath=None):
 		"""
 		>>> db = FilesDb()
-		>>> for drive in db.exclude_drives:
-		... 	print drive,
+		>>> print ' '.join(db.exclude_drives)
 		C
 		>>> print db.db_filepath
 		None
@@ -26,24 +25,30 @@ class FilesDb:
 		self._init_db()
 		self.size_not_found = []
 		
-	def _add_fake_file(self, drive, file=None, size=None):
-		""" add fake file in database for test purpose only
-		>>> FilesDb()._add_fake_file('A')._add_fake_file('A').len('A')
-		2
-		"""
-		if drive not in self._db: self._db[drive] = []
-		files = self._db[drive]
-		if file is None:
-			if files: i = int(files[-1][1].split()[1]) + 1
-			else: i = 1
-			file = 'file {}'.format(i)
-		if size is None: size = 0
-		files.append(( join(drive+':/', 'dir'), file, size ))
-		return self
+	#def _add_fake_file(self, drive, file=None, size=None):
+	#	""" add fake file in database for test purpose only
+	#	>>> FilesDb()._add_fake_file('A')._add_fake_file('A').len('A')
+	#	2
+	#	"""
+	#	if drive not in self._db: self._db[drive] = []
+	#	files = self._db[drive]
+	#	if file is None:
+	#		if files: i = int(files[-1][1].split()[1]) + 1
+	#		else: i = 1
+	#		file = 'file {}'.format(i)
+	#	if size is None: size = 0
+	#	files.append(( join(drive+':/', 'dir'), file, size ))
+	#	return self
 		
 	def _init_db(self):
 		"""
-		>>> db = FilesDb()._add_fake_file('A')._add_fake_file('A')
+		>>> db = FilesDb()
+		>>> db._init_db()
+		>>> db._db = 'toto'
+		>>> db._init_db()
+		>>> db._db
+		{}
+		>>> db._db = dict(A='toto')
 		>>> db._init_db()
 		>>> db._db
 		{}
@@ -54,15 +59,17 @@ class FilesDb:
 		""" database file count
 		>>> len(FilesDb())
 		0
+		>>> len(testdb)
+		9
 		"""
 		return self.len()
 	
 	def len(self, drive=None):
 		""" file count of database or drive
-		>>> FilesDb().len('C')
+		>>> FilesDb().len('X')
 		0
-		>>> FilesDb().len()
-		0
+		>>> testdb.len(drive1)
+		5
 		"""
 		if drive is None: return sum([len(files) for files in self._db.itervalues()])
 		elif len(drive) == 1 and drive in self._db: return len(self._db[drive])
@@ -72,6 +79,14 @@ class FilesDb:
 		""" load database from self.db_filepath using pickle -> count of file entries loaded
 		>>> FilesDb().load()
 		0
+		>>> fd, path = tempfile.mkstemp()
+		>>> os.close(fd)
+		>>> testdb.db_filepath = path
+		>>> testdb.save()
+		True
+		>>> FilesDb(db_filepath=path).load()
+		9
+		>>> os.remove(path)
 		"""
 		if self.db_filepath is None: return 0
 		try:
@@ -83,10 +98,15 @@ class FilesDb:
 			return 0
 
 	def save(self):
-		"""
-		save database in self.db_filepath using pickle -> success (boolean)
+		""" save database in self.db_filepath using pickle -> success (boolean)
 		>>> FilesDb().save()
 		False
+		>>> fd, path = tempfile.mkstemp()
+		>>> os.close(fd)
+		>>> testdb.db_filepath = path
+		>>> testdb.save()
+		True
+		>>> os.remove(path)
 		"""
 		if self.db_filepath is None: return False
 		try:
@@ -97,17 +117,23 @@ class FilesDb:
 			return False
 		
 	def clear(self, drive=None):
-		"""
-		clear database or drive -> success (boolean)
+		""" clear database or drive -> success (boolean)
 		>>> db = FilesDb()
 		>>> db.clear()
 		True
 		>>> db.clear('A')
 		False
-		>>> db._add_fake_file('A').clear('A')
+		>>> db._db = dict(A=[('', '', 0), ('', '', 0)], B=[('', '', 0)])
+		>>> len(db)
+		3
+		>>> db.clear('A')
 		True
-		>>> db.drives()
-		[]
+		>>> len(db)
+		1
+		>>> db.clear()
+		True
+		>>> len(db)
+		0
 		"""
 		if drive is None: self._init_db()
 		elif len(drive) == 1 and drive in self._db: del self._db[drive]
@@ -118,44 +144,43 @@ class FilesDb:
 		""" sorted copy of database drives list
 		>>> FilesDb().drives()
 		[]
-		>>> FilesDb()._add_fake_file('D')._add_fake_file('C').drives()
-		['C', 'D']
+		>>> testdb.drives() == sorted([drive1, drive2])
+		True
 		"""
 		return sorted(self._db.iterkeys())
 				
 	def files(self, drive=None):
-		""" generator on stored files
-		>>> def _print_sizes(files):
-		... 	for root, file, size in files:
-		... 		print size,
-		>>> db = FilesDb()
-		>>> _print_sizes(db.files())
-		
-		>>> _print_sizes(db.files('A'))
-		
-		>>> db = db._add_fake_file('A')._add_fake_file('B')
-		>>> _print_sizes(db.files())
-		0 0
-		>>> _print_sizes(db.files('A'))
-		0
+		r""" generator on stored files
+		>>> for root, file, size in testdb.files():
+		... 	print root[1:11]
+		:\0_Tout e
+		:\1_L'amou
+		:\2_Ils n'
+		:\3_Et leu
+		:\
+		:\0_Votre 
+		:\1_Que vo
+		:\2_Jouant
+		:\3_Triste
+		>>> for root, file, size in testdb.files(drive2):
+		... 	print root[1:11]
+		:\0_Tout e
+		:\1_L'amou
+		:\2_Ils n'
+		:\3_Et leu
 		"""
 		drives = self.drives()
 		if drive != None:
 			if drive in drives: drives = [drive]
 			else: drives = []
-		for d in drives:
+		for d in sorted(drives):
 			for file in self._db[d]:
 				yield file
 				
 	def size(self, drive=None):
 		""" sum of stored files size
-		>>> FilesDb().size()
-		0
-		>>> db = FilesDb()._add_fake_file('A', size=2)._add_fake_file('B', size=3)
-		>>> db.size()
-		5
-		>>> db.size('B')
-		3
+		>>> print testdb.size(), testdb.size(drive1), testdb.size(drive2)
+		13 13 0
 		"""
 		s = 0
 		for root, file, size in self.files(drive):
@@ -176,20 +201,14 @@ class FilesDb:
 
 	def _scan_drive_gen(self, drive):
 		r""" 
-		>>> import files_db_test as test
-		>>> drive, dir = test.make_drive()
-		>>> path = '0_Votre âme/est un/ paysage/choisi;1_Que vont/charmant masques/et bergamasques;2_Jouant du luth/et dansant/ et quasi;3_Tristes/sous leurs déguisements/ fantasques._'.decode('utf8')
-		>>> test.touch(path.split(';'), drive+':/')
-		>>> #
 		>>> db = FilesDb()
-		>>> for root, file, size in db._scan_drive_gen(drive):
+		>>> for root, file, size in db._scan_drive_gen(drive1):
 		... 	print root.replace('â'.decode('utf8'), 'A').replace('é'.decode('utf8'), 'E')[1:], file, size
+		:\ titre.txt 13
 		:\0_Votre Ame\est un\ paysage choisi 0
 		:\1_Que vont\charmant masques et bergamasques 0
 		:\2_Jouant du luth\et dansant  et quasi 0
 		:\3_Tristes\sous leurs dEguisements  fantasques._ 0
-		>>> #
-		>>> test.del_drive(drive, dir)
 		"""
 		for root, dirs, files in os.walk(drive + u':\\'):
 			for file in files:
@@ -202,21 +221,12 @@ class FilesDb:
 				yield (root, file, size)
 				
 	def update_drives_gen(self, drives):
-		r""" 
-		>>> import files_db_test as test
-		>>> drive1, dir1 = test.make_drive()
-		>>> path = '0_Votre âme/est un/ paysage/choisi;1_Que vont/charmant masques/et bergamasques;'.decode('utf8') \
-		... + '2_Jouant du luth/et dansant/ et quasi;3_Tristes/sous leurs déguisements/ fantasques._'.decode('utf8')
-		>>> test.touch(path.split(';'), drive1+':/')
-		>>> drive2, dir2 = test.make_drive()
-		>>> path = "0_Tout en chantant/sur le mode.mineur;1_L'amour/vainqueur/et la vie/opportune;" \
-		... + "2_Ils n'ont pas/l'air de croire/à leur bonheur;3_Et leur chanson/se mêle/au clair de lune,".decode('utf8')
-		>>> test.touch(path.split(';'), drive2+':/')
-		>>> #
+		r"""
 		>>> db = FilesDb()
 		>>> for root, file, size in db.update_drives_gen(drive1+drive2):
 		... 	print root.replace('â'.decode('utf8'), 'A1').replace('é'.decode('utf8'), 'E1').replace('ê'.decode('utf8'), 'E2')[1:],
 		... 	print '-', file.replace('à'.decode('utf8'), 'A2'), size
+		:\ - titre.txt 13
 		:\0_Votre A1me\est un\ paysage - choisi 0
 		:\1_Que vont\charmant masques - et bergamasques 0
 		:\2_Jouant du luth\et dansant -  et quasi 0
@@ -227,11 +237,8 @@ class FilesDb:
 		:\3_Et leur chanson\se mE2le - au clair de lune, 0
 		>>> db.drives() == [drive2, drive1]
 		True
-		>>> db.len(drive1)
+		>>> db.len(drive2)
 		4
-		>>> #
-		>>> test.del_drive(drive1, dir1)
-		>>> test.del_drive(drive2, dir2)
 		"""
 		for drive in drives:
 			first = True
@@ -243,29 +250,12 @@ class FilesDb:
 				yield file
 			
 	def update_drives(self, drives):
-		r""" 
-		>>> import files_db_test as test
-		>>> drive1, dir1 = test.make_drive()
-		>>> path = '0_Votre âme/est un/ paysage/choisi;1_Que vont/charmant masques/et bergamasques;'.decode('utf8') \
-		... + '2_Jouant du luth/et dansant/ et quasi;3_Tristes/sous leurs déguisements/ fantasques._'.decode('utf8')
-		>>> test.touch(path.split(';'), drive1+':/')
-		>>> drive2, dir2 = test.make_drive()
-		>>> path = "0_Tout en chantant/sur le mode.mineur;1_L'amour/vainqueur/et la vie/opportune;" \
-		... + "2_Ils n'ont pas/l'air de croire/à leur bonheur;3_Et leur chanson/se mêle/au clair de lune,".decode('utf8')
-		>>> test.touch(path.split(';'), drive2+':/')
-		>>> with open(os.path.join(drive1+':/', 'titre.txt'), 'w') as file:
-		... 	file.write('Clair de lune')
-		>>> #
+		r"""
 		>>> db = FilesDb()
 		>>> db.update_drives(drive1+drive2)
 		9
-		>>> db.drives() == [drive2, drive1]
-		True
-		>>> db.size(drive1) > 0
-		True
-		>>> #
-		>>> test.del_drive(drive1, dir1)
-		>>> test.del_drive(drive2, dir2)
+		>>> print db.size(drive1)
+		13
 		"""
 		self.size_not_found = []
 		c = 0
@@ -274,10 +264,10 @@ class FilesDb:
 
 	def update_gen(self):
 		"""
-		>>> db = FilesDb(exclude_drives='')
-		>>> db.exclude_drives = db._get_drives()
-		>>> [x for x in db.update_gen()]
-		[]
+		>>> db = FilesDb(exclude_drives=testdb.exclude_drives)
+		>>> for size in sorted([size for root, file, size in db.update_gen()]):
+		... 	print size,
+		0 0 0 0 0 0 0 0 13
 		"""
 		self.size_not_found = []
 		drives = self._get_drives()
@@ -286,10 +276,9 @@ class FilesDb:
 			
 	def update(self):
 		"""
-		>>> db = FilesDb(exclude_drives='')
-		>>> db.exclude_drives = db._get_drives()
+		>>> db = FilesDb(exclude_drives=testdb.exclude_drives)
 		>>> db.update()
-		0
+		9
 		"""
 		c = 0
 		for file in self.update_gen(): c += 1
@@ -297,36 +286,20 @@ class FilesDb:
 		
 	# input : can contain '*' to enlarge search
 	def find(self, input, ignore_case=False): # -> [ ( root, file, size ), ... ]
-		r""" 
-		>>> import files_db_test as test
-		>>> drive1, dir1 = test.make_drive()
-		>>> path = '0_Votre âme/est un/ paysage/choisi;1_Que vont/charmant masques/et bergamasques;'.decode('utf8') \
-		... + '2_Jouant du luth/et dansant/ et quasi;3_Tristes/sous leurs déguisements/ fantasques._'.decode('utf8')
-		>>> test.touch(path.split(';'), drive1+':/')
-		>>> drive2, dir2 = test.make_drive()
-		>>> path = "0_Tout en chantant/sur le mode.mineur;1_L'amour/vainqueur/et la vie/opportune;" \
-		... + "2_Ils n'ont pas/l'air de croire/à leur bonheur;3_Et leur chanson/se mêle/au clair de lune,".decode('utf8')
-		>>> test.touch(path.split(';'), drive2+':/')
-		>>> #
-		>>> db = FilesDb()
-		>>> db.update_drives(drive1+drive2)
-		8
-		>>> for root, file, size in db.find('*._'):
+		r"""
+		>>> for root, file, size in testdb.find('*._'):
 		... 	print file
 		 fantasques._
-		>>> for root, file, size in sorted(db.find(' *')):
+		>>> for root, file, size in sorted(testdb.find(' *')):
 		... 	print file
 		 et quasi
 		 fantasques._
-		>>> for root, file, size in sorted(db.find('*de*')):
+		>>> for root, file, size in sorted(testdb.find('*de*')):
 		... 	print file
 		sur le mode.mineur
 		au clair de lune,
-		>>> len(db.find('*'))
-		8
-		>>> #
-		>>> test.del_drive(drive1, dir1)
-		>>> test.del_drive(drive2, dir2)
+		>>> len(testdb.find('*'))
+		9
 		"""
 		if not input: return []
 		if ignore_case: elmts = input.lower().split('*')
@@ -363,5 +336,31 @@ class FilesDb:
 		return matches
 
 if __name__ == "__main__":
-    import doctest, sys
-    doctest.testmod(verbose='-v' in sys.argv)
+	import doctest, sys, tempfile
+	import files_db_test as test
+	drive1, dir1 = test.make_drive()
+	path = '0_Votre âme/est un/ paysage/choisi;1_Que vont/charmant masques/et bergamasques;'.decode('utf8') + \
+		'2_Jouant du luth/et dansant/ et quasi;3_Tristes/sous leurs déguisements/ fantasques._'.decode('utf8')
+	test.touch(path.split(';'), drive1+':/')
+	drive2, dir2 = test.make_drive()
+	path = "0_Tout en chantant/sur le mode.mineur;1_L'amour/vainqueur/et la vie/opportune;" + \
+		"2_Ils n'ont pas/l'air de croire/à leur bonheur;3_Et leur chanson/se mêle/au clair de lune,".decode('utf8')
+	test.touch(path.split(';'), drive2+':/')
+	with open(os.path.join(drive1+':/', 'titre.txt'), 'w') as file:
+		file.write('Clair de lune')
+		
+	ex = [c for c in string.uppercase if c not in drive1+drive2]
+	updated_db = FilesDb(exclude_drives=ex)
+	c = updated_db.update_drives(drive1+drive2)
+	if c == 0:
+		raise Exception('test preparation failed !')
+
+	doctest.testmod(
+		name='files_db',
+		extraglobs=dict(drive1=drive1, drive2=drive2, testdb=updated_db),
+		optionflags=doctest.REPORT_ONLY_FIRST_FAILURE # | doctest.REPORT_UDIFF
+		)
+	
+	test.del_drive(drive1, dir1)
+	test.del_drive(drive2, dir2)
+	
